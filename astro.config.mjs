7701c7470e11a327dc9d7371f0d44eece2fb1e65
Amanda Silveira <82608627+amsilveira-ce@users.noreply.github.com>
2025-12-09 10:14:52 -0300

    Change base path to '/blog' in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://amsilveira-ce.github.io',
-  base: '/',
+  base: '/blog',
   integrations: [mdx(), sitemap(), tailwind()]
 });
